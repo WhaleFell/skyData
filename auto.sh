0b251a12e62eac0fe1aac738a3430e3024a39534
adminwhalefall <2734184475@qq.com>
2021-02-19 11:43:22 +0800

    2021-02-19 11:43:22 自动更新提交Sky_CSV文件

--- auto.sh
+++ auto.sh
@@ -5,7 +5,7 @@ path_shell=$(dirname $(readlink -f "$0"))
 echo "脚本目录:"$path_shell""
 
 cd $path_shell
-python3 sky.py
+# python3 sky.py
 
 if [ "$?" != "0" ];then
     echo "运行 sky.py 出现错误!"
@@ -21,7 +21,8 @@ echo "##########提交GitHub#############"
 git add .
 time=$(date "+%Y-%m-%d %H:%M:%S")
 git commit -m "${time} 自动更新提交Sky_CSV文件"
-git_log=$(git push -u github master 2>&1)
+# git_log=$(git push -u github master 2>&1)
+cat /ectas/sasasa
 if [ "$?" != "0" ];then
     content=""${time}"\n[skyData]推送到GitHub出现错误!\n"${git_log}""
     echo ${content}
