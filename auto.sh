cbadcbcb79153be2776731255d57bb767191b6e3
adminwhalefall <2734184475@qq.com>
2021-02-19 11:36:29 +0800

    2021-02-19 11:36:29 自动更新提交Sky_CSV文件

--- auto.sh
+++ auto.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-source /etc/profile
+# 酷推token
+token="92f83d0596c7b553ea1df9f242e4fc46"
 path_shell=$(dirname $(readlink -f "$0"))
 echo "脚本目录:"$path_shell""
 
@@ -20,9 +21,11 @@ echo "##########提交GitHub#############"
 git add .
 time=$(date "+%Y-%m-%d %H:%M:%S")
 git commit -m "${time} 自动更新提交Sky_CSV文件"
-git push -u github master
+git_log=$(git push -u github master 2>&1)
 if [ "$?" != "0" ];then
-    echo "推送到GitHub出现错误!"
+    content=""${time}"\n推送到GitHub出现错误!\n"${git_log}""
+    echo ${content}
+    curl "https://push.xuthus.cc/send/"${token}"?c="${content}""
     exit 1
 fi
 
