fea1d3455baa74f96a47c4634afd6724411dbee4
adminwhalefall <2734184475@qq.com>
2021-02-19 15:31:30 +0800

    2021-02-19 15:31:30 自动更新提交Sky_CSV文件

--- push.py
+++ push.py
@@ -18,6 +18,6 @@ elif status == "False":
 else:
     raise TypeError("参数有误")
 
-data = "%s" % (content)
+data = "%s".encode('utf-8') % (content)
 res = requests.post(url=url, data=data)
 print(res.text)
